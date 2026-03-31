1f8e3602b557a06c6cc248a935f13fb2fe43ef76
Ander-tsx <20233tn107@utez.edu.mx>
2026-03-30 23:33:58 -0600

    [FEAT]: Añadi la configuracion de usuario personalizado basado en el usuario base que da django

--- backend/dystoai/settings.py
+++ backend/dystoai/settings.py
@@ -115,6 +115,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
